0516e404be6cfd863689b4e8d349be41d588ed32
Kyle Robinson Young <kyle@dontkry.com>
2013-11-20 11:06:59 -0800

    0.2.0

---
+++ README.md
@@ -0,0 +1,116 @@
+# ember-webpack-resolver
+
+> An Ember.js resolver heavily inspired by
+https://github.com/stefanpenner/ember-jj-abrams-resolver but mainly for use with webpack.
+
+*Please view [Single Page Apps with Webpack](http://dontkry.com/posts/code/single-page-apps-with-webpack.html) for more information on why this exists.*
+
+## Install
+
+``` shell
+npm install ember-webpack-resolver --save-dev
+```
+
+## Usage
+
+This resolver is intended to resolve modules with a folder structure like such:
+
+```
+| - app/
+| --- components/
+| --- controllers/
+| --- models/
+| --- routes/
+| --- templates/
+| --- views/
+| --- app.js
+| --- router.js
+| - node_modules/
+| --- some-widget-ember-component
+| ----- index.js
+| ----- index.hbs
+```
+
+A very simple config will resolve just your local modules:
+
+``` javascript
+var App = Ember.Application.create({
+  Resolver: require('ember-webpack-resolver')({ context: require })
+});
+```
+
+If you're using a file extension other than `.js`, supply the lookup extensions such use with coffeescript:
+
+``` javascript
+var App = Ember.Application.create({
+  Resolver: require('ember-webpack-resolver')({
+    context: require,
+    extensions: ['.coffee', '.hbs'],
+  })
+});
+```
+
+### Resolving Components
+If you want to also resolve modules within vendor folders, a bit more configuration is required:
+
+``` javascript
+var App = Ember.Application.create({
+  Resolver: require('ember-webpack-resolver')({
+    context: require,
+    component: [{
+      context: require.context('../node_modules/', true, /(.+)-ember-component\/index/),
+      format: '%@-ember-component/index'
+    }]
+  })
+});
+```
+
+This will look for modules within the `node_modules/` folder that end with `-ember-component/index`.
+
+In order to properly encapsulate components and their templates, this resolver assumes a specific module format of your vendor components:
+
+``` javascript
+// node_modules/some-widget-ember-component/index.js
+module.exports = function(template) {
+  if (!Ember.TEMPLATES[template]) Ember.TEMPLATES[template] = require('./index.hbs');
+
+  return Ember.Component.extend({
+    classNames: ['some-widget']
+  });
+};
+```
+
+``` html
+<!-- node_modules/some-widget-ember-component/index.hbs -->
+<div {{bindAttr width="width" height="height"}}>SOME WIDGET</div>
+```
+
+The above method is verbose but it makes your components more flexible.
+
+You can explicitly require your components and dynamically set the template name:
+
+``` javascript
+App.AnotherWidgetComponent = require('some-widget-ember-component')('components/another-widget');
+```
+
+Or if you want to extend another component and package it:
+
+``` javascript
+// node_modules/enhanced-widget-ember-component/index.js
+module.exports = function(template) {
+  var SomeWidget = require('some-widget-ember-component')(template);
+  return SomeWidget.extend({
+    classNames: ['enhanced-widget']
+  });
+};
+```
+
+Hooray! Shareable, nested, auto-resolving Ember components!
+
+## Release History
+* 0.2.0 - handle nested components, update API
+* 0.1.0 - initial release
+
+## License
+Copyright (c) 2013 Kyle Robinson Young  
+Licensed under the MIT license.
---
+++ index.js
@@ -0,0 +1,135 @@
+module.exports = function(options) {
+  options = options || {};
+  options.modulePrefix = options.modulePrefix || './';
+  options.extensions = options.extensions || ['.js', '.hbs'];
+  options.fixToString = options.fixToString !== false;
+  options.has = options.has || options.context.keys();
+
+  // Find modules in map for dynamic context requires
+  function findInMap(parsedName, callback) {
+    if (!options[parsedName.type]) return false;
+    var map = options[parsedName.type];
+    if (!Array.isArray(map)) map = [map];
+    for (var i = 0; i < map.length; i++) {
+      var context = map[i].context;
+      var find = './' + Ember.String.fmt(map[i].format, [parsedName.fullNameWithoutType]);
+      var keys = context.keys();
+      if (keys.indexOf(find) !== -1) {
+        return callback(find, context);
+      }
+    }
+    return false;
+  }
+
+  function parseName(fullName) {
+    var nameParts = fullName.split(':');
+    return {
+      fullName: fullName,
+      type: nameParts[0],
+      fullNameWithoutType:  nameParts[1],
+      name:  nameParts[1],
+      root: Ember.get(this, 'namespace'),
+      resolveMethodName: 'resolve' + Ember.String.classify(nameParts[0])
+    };
+  }
+
+  function resolveOther(parsedName) {
+    if (!parsedName.name || !parsedName.type) {
+      return this._super.apply(this, arguments);
+    }
+
+    var factory;
+    var moduleName = false;
+    var variations = [options.modulePrefix + parsedName.type + 's/' + parsedName.fullNameWithoutType];
+    var contextrequire = options.context;
+
+    if (Array.isArray(options.has)) {
+      for (var i = 0; i < options.extensions.length; i++) {
+        variations.push(variations[0] + options.extensions[i]);
+      }
+      for (var i = 0; i < variations.length; i++) {
+        if (options.has.indexOf(variations[i]) !== -1) {
+          moduleName = variations[i];
+          break;
+        }
+      }
+    } else {
+      moduleName = variations[0];
+    }
+
+    // If module not found, look in the map
+    if (moduleName === false) {
+      moduleName = findInMap(parsedName, function(name, context) {
+        // Handle module.exports = function(template) {}; module format
+        if (parsedName.type === 'component') {
+          context = context(name);
+          name = 'components/' + parsedName.fullNameWithoutType;
+        }
+        return [name, context];
+      });
+      if (moduleName !== false) {
+        contextrequire = moduleName[1];
+        moduleName = moduleName[0];
+      }
+    }
+
+    // Module not found, return the parent
+    if (moduleName === false) {
+      if (Ember.ENV.LOG_MODULE_RESOLVER) {
+        Ember.Logger.info('miss', parsedName.fullName);
+      }
+      return this._super.apply(this, arguments);
+    }
+
+    try {
+      factory = contextrequire(moduleName);
+    } catch (err) {
+      if (Ember.ENV.LOG_MODULE_RESOLVER) {
+        Ember.Logger.info('miss', moduleName);
+      }
+      return this._super.apply(this, arguments);
+    }
+
+    if (factory === undefined) {
+      throw new Error(' Expected to find: "' + parsedName.fullName + '" within "' + moduleName + '" but got "undefined". Did you forget to `module.exports` within "' + moduleName + '"?');
+    }
+
+    // To fix class introspection
+    if (options.fixToString) {
+      var className = factory.toString();
+      if (className.indexOf('(') !== -1) {
+        factory.toString = function() {
+          var type = Ember.String.classify(parsedName.type);
+          if (type === 'Model') type = '';
+          return 'App.' + Ember.String.classify(parsedName.fullNameWithoutType + type);
+        };
+      }
+    }
+
+    if (Ember.ENV.LOG_MODULE_RESOLVER) {
+      Ember.Logger.info('hit', moduleName);
+    }
+    return factory;
+  }
+
+  function resolveRouter(parsedName) {
+    if (parsedName.fullName === 'router:main') {
+      return options.context(options.modulePrefix + 'router');
+    }
+    return this._super.apply(this, arguments);
+  }
+
+  return Ember.DefaultResolver.extend({
+    resolveOther: resolveOther,
+    resolveTemplate: resolveOther, // TODO: Check Ember.TEMPLATES as backup
+    resolveRouter: resolveRouter,
+    parseName: parseName,
+    normalize: function(fullName) {
+      // replace `.` with `/` in order to make nested controllers work in the following cases
+      // 1. `needs: ['posts/post']`
+      // 2. `{{render 'posts/post'}}`
+      // 3. `this.render('posts/post')` from Route
+      return Ember.String.dasherize(fullName.replace(/\./g, '/'));
+    }
+  });
+};
